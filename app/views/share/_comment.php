df8653d0de118813f584c6f36b88d56022428bef
Vincent Alquier <vincent.alquier@gmail.com>
2010-02-15 21:02:14 +0100

    issue #11: html links in comments

--- app/views/share/_comment.php
+++ app/views/share/_comment.php
@@ -1,13 +1,15 @@
 
 <?php
     $author = $comment->author->target();
+    $body = nl2br(htmlspecialchars($comment->body));
+    $body = preg_replace('/http(s){0,1}:\/\/([^\s\(\)\{\}\|\\\^~\[\]<`]*)/e','\'<a href="http\1://\'.str_replace(\'"\',\'%22\',\'\2\').\'">\2</a>\'',$body);
 ?>
 
 <div class="comment">
     <div class="gravatar"><?= gravatar_tag($author); ?></div>
     <div class="details">
         <span class="author"><?= _f('Commented by %s, %s :', array($author, displayable_date($comment->created_on) )) ?></span>
-        <blockquote><?= nl2br(htmlspecialchars($comment->body)) ?></blockquote>
+        <blockquote><?= $body ?></blockquote>
     </div>
 </div>
 
